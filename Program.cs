3416936af4a4d1fd8c7d87dbec99e760c952e984
cirosanchez <ciroandressanchezbuitrago@gmail.com>
2023-10-24 15:26:26 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace test {
+    public class Program {
+        static void Main(string[] args) {
+            float[] array = new float[3];
+            array[0] = 4.6f;
+            array[2] = 5.6f;
+            array[1] = 7.0f;
+
+            Console.WriteLine(factorial(3));
+            average(array);
+        }
+       private static int factorial(int x) {
+
+            int result = 1;
+            for (int i = 1; i <= x; i++) {
+                result = i*result;
+            }
+            return result;
+        }
+        
+        private static void average(float[] arr) {
+            float average = 0;
+            for (int i = 0; i < arr.Length; i++) {
+                average = average+ arr[i];
+            }
+            Console.WriteLine(average / arr.Length);
+        }
+
+    }
+}
